b5a0318d92d9216ac094b7d871a2e323444c7012
Abouelwafa Choaib <cabouelw@student.1337.ma>
2023-08-22 18:03:59 +0100

    add AOS animation and fix the problem of snap scroll with AOS

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,39 @@ import { StrictMode } from 'react'
 import AppWrapper from '../components/AppWrapper'
 import '../styles/globals.css'
 import { ThemeProvider } from "@material-tailwind/react";
+import aos from 'aos'
+import 'aos/dist/aos.css'
+import "../styles/globals.css";
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
-  return <StrictMode><ThemeProvider><AppWrapper><Component {...pageProps} /></AppWrapper></ThemeProvider></StrictMode>
+  useEffect(() => {
+    aos.init({
+      duration: 1000,
+      once: true,
+    });
+    const aosAnimation = document.querySelectorAll('[data-aos]');
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+          entry.target.classList.add('aos-animate');
+        } else {
+          entry.target.classList.remove('aos-animate');
+        }
+      });
+    });
+    aosAnimation.forEach(aosObject => {
+      observer.observe(aosObject);
+    });
+
+  }, [])
+  return <StrictMode>
+    <ThemeProvider>
+      <AppWrapper>
+        <Component {...pageProps} />
+      </AppWrapper>
+    </ThemeProvider>
+  </StrictMode>
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -7,13 +7,12 @@ import ProjectSection from '../components/Home/projectSection'
 
 export default function Home() {
 	return (
-		<div className='grid gap-16 justify-items-center w-full max-w-7xl  px-10 my-20 '>
-			<title>Choaib Abouelwafa</title>
+		<>
 			<FirstSection />
 			<ProjectSection />
 			<SkillsSection />
 			<AboutMeSection />
 			<ContactSection />
-		</div>
+		</>
 	)
 }
